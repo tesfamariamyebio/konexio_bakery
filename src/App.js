8adf80120a4947267ca065a49589615169f1256f
tesfamariamyebio <65938837+tesfamariamyebio@users.noreply.github.com>
2020-07-22 16:36:18 +0200

    day5bakery

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+class App extends React.Component{
+  render(){
+    return(
+      <div classsName=""> 
+      </div>
+    );
+  }
+}
+export default App;
