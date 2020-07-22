433ce88488d6f7b1b255149d0ee53f338aeb2a6a
tesfamariamyebio <65938837+tesfamariamyebio@users.noreply.github.com>
2020-07-22 18:16:13 +0200

    day5bakery

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import Button from  './core/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    // this.state={
+    //   activeTab:add,
+    //   items:[],
+    // };
+  }
+  renderAdd () {
+    return (
+    <div>add</div>
+    );
+  }
+  renderList(){
+    return (
+      <div>list</div>
+    );
+  }
+  renderPay(){
+    return (
+      <div>pay</div>
+    );
+  }
   render(){
     return(
-      <div classsName=""> 
+      <div classsName="boulanger"> 
+     {this.renderAdd()} 
+      {this.renderList()}
+      {this.renderPay()}
       </div>
     );
   }
 }
+
 export default App;
